d40aef0b454aa5872f6f0a8b3565aff1099614a4
le quang dao <qdao98nd@gmail.com>
2022-08-09 22:33:26 +0700

    create project

---
+++ src/main/java/com/example/cineflix/controller/HomeController.java
@@ -0,0 +1,29 @@
+package com.example.cineflix.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+	
+	@GetMapping("")
+	public ModelAndView home() {
+		ModelAndView andView = new  ModelAndView("home");
+		
+		int soA = 10;
+		int soB = 20;
+		
+		int tong = tinhTong(soA, soB);
+		
+		andView.addObject("ketqua", tong);	
+		return andView;
+	}
+	
+	public int tinhTong(int soA, int soB) {
+		int tong = soA + soB;
+		return tong;
+	}
+}
